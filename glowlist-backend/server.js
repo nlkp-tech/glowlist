f6aa2fbdcf1a7743013f3c578818c44d7c63495a
nlkp-tech <nadinlatifa62@gmail.com>
2026-08-11 09:57:38 +0700

    update server.js

---
+++ glowlist-backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const mysql2 = require('mysql2');
+
+const db = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database Glowlist');
+    }
+})
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Selamat Datang di Glowlist!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Glowlist jalan di http://localhost:${PORT}`);
+});
+
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
